e08a60816af26944ec544af80119cfefc77f566d
dennis feehan <feehan@berkeley.edu>
2021-01-27 22:08:16 -0800

    test lab02

---
+++ lab02/tests/country_e0.py
@@ -0,0 +1,17 @@
+test = {   'name': 'country_e0',
+    'points': 3,
+    'suites': [   {   'cases': [   {   'code': '>>> e0_2015.num_rows == 201\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> '
+                                               "np.isclose(e0_2015.where('country', "
+                                               "are.equal_to('Aruba'))['e0_m'][0], "
+                                               '72.858401)\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
